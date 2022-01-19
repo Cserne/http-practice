09ee934e275f3f64a89ddc01f6a8dca6dec56edb
Cserne <angeli.cserne@gmail.com>
2022-01-19 10:14:54 +0100

    Changes added.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("Hello World!");
+
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('http://nemzetisport.hu')
+  .then(function (response) {
+    // handle success
+    console.log(response.status);
+    console.log(response.headers);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
